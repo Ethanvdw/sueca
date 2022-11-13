c2733c960f27b73a472d78eef22dbcd371479580
Ethanvdw <ethan.westhuizen@gmail.com>
2022-11-13 18:26:40 +0000

    Update game.py

---
+++ game.py
@@ -0,0 +1,13 @@
+def run_game(fname: str, showCards: bool, showGame: bool) -> None:
+    '''
+    Takes the name of a game file, and two boolean values.
+    The first boolean indicates whether the cards should be shown to the console.
+    The second boolean indicates whether the game should be shown to the console.
+
+    This function parses the game file and runs the game. It indicates the score and winning pair at the end of the game.
+    '''
+    pass
+
+
+class Game():
+    pass
